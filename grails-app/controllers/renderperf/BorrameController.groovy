adf9a5bed767ea0d09d9039aad6a2501f71e0751
dnoseda <damian.noseda@mercadolibre.com>
2010-09-09 13:00:23 -0300

    inicial

---
+++ grails-app/controllers/renderperf/BorrameController.groovy
@@ -0,0 +1,21 @@
+package vip
+
+class BorrameController {
+
+    def index = {
+		render(view:"/vips/borrameView")
+	}
+	
+	def index2 = {
+		render "index2"
+	}
+	def index3 = {
+		render "index3"
+	}
+	def index4 = {
+		render "index4"
+	}
+	def index5 = {
+		render "index5"
+	}
+}
---
+++ grails-app/views/borrame/index.gsp
@@ -0,0 +1,7 @@
+<ol>
+  <li>${existsLayout}</li>
+  <li>${existsView}</li>
+  <li>${view}</li>
+</ol>
+
+<debugToolbar:renderMe/>
